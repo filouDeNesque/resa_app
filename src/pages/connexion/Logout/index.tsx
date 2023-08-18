9afcb1a29a511f2041fde7dae5557f2e9e8ffa21
p.cuquemy@yahoo.com <p.cuquemy@yahoo.com>
2023-08-18 11:20:42 +0200

    refactor logout page

--- src/pages/connexion/Logout/index.tsx
+++ src/pages/connexion/Logout/index.tsx
@@ -1,13 +1,13 @@
 import { signOut } from "next-auth/react";
 
-export default function DiscConnect() {
+export default function Logout() {
   return (
     <div className="flex h-screen items-center justify-center">
       <button
         className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
         onClick={() => void signOut()}
       >
-        Se connecter avec Google
+        Logout
       </button>
     </div>
   );
