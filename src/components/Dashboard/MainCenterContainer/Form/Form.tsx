0a2aaa983e90b1dfb9a9c8c61f1b094937fad476
filouDeNesque <p.cuquemy@yahoo.com>
2023-08-31 14:57:48 +0200

    form refactor

--- src/components/Dashboard/MainCenterContainer/Form/Form.tsx
+++ src/components/Dashboard/MainCenterContainer/Form/Form.tsx
@@ -1,6 +1,7 @@
 import React, { type FormEvent } from "react";
 import { useHorseData } from "~/hooks/useHorseData";
 import { type Horse } from "~/types/Horse.type";
+import { Stabs } from "~/types/stabs";
 
 interface FormProps {
     onSubmit: (horse: Horse) => void;
@@ -39,33 +40,38 @@ const Form: React.FC<FormProps> = ({ onSubmit }) => {
         onSubmit(horse);
     };
 
+
+    interface inputLabel {
+        name: string;
+        type: string;
+        id: string;
+        htmlFor: string;
+    }
+    interface itemForm {
+        type: string;
+        inputlabel: inputLabel;
+        horse?: Horse;
+        stab?: Stabs;
+        halfLeaseUsers?: Horse;
+        createHorse: () => void;
+        updateHorse: () => void;
+        createStab: () => void;
+        updateStab: () => void;
+    }
+
+
     return (
 
         <form onSubmit={handleSubmit}>
             <div className="grid md:grid-cols-2 md:gap-6">
-                <div className="relative z-0 w-full mb-6 group">
-                    <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
-                </div>
-                <div className="relative z-0 w-full mb-6 group">
-                    <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
-                </div>
+                <FormField label="Name" name="floating_first_name" placeholder="" type="text" />
+                <FormField label="Last Name" name="floating_last_name" placeholder="" type="text" />
             </div>
             <div className="grid md:grid-cols-2 md:gap-6">
-                <div className="relative z-0 w-full mb-6 group">
-                    <input type="number" step="0.01" name="floating_size" id="floating_size" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_size" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Size</label>
-                </div>
-                <div className="relative z-0 w-full mb-6 group">
-                    <input type="date" name="floating_birthDate" id="floating_birthDate" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label htmlFor="floating_birthDate" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Birth Date</label>
-                </div>
+                <FormField label="Size" name="floating_size" placeholder="" type="number" step="0.01" />
+                <FormField label="Birth Date" name="floating_birthDate" placeholder="" type="date" />
             </div>
-            <div className="relative z-0 w-full mb-6 group">
-                <input type="text" name="stab" id="floating_stab" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" Ecurie de Londre" required />
-                <label htmlFor="floating_stab" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Stab</label>
-            </div>
+            <FormField label="Stab" name="floating_stab" placeholder="" type="text" />
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
 
@@ -73,3 +79,11 @@ const Form: React.FC<FormProps> = ({ onSubmit }) => {
 };
 
 export default Form;
+
+
+const FormField: React.FC<{ label: string, name: string, type: string, placeholder: string, required?: boolean, step?: string }> = ({ label, name, type, placeholder, required, step }) => (
+    <div className="relative z-0 w-full mb-6 group">
+        <input type={type} name={name} id={name} step={step} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder={` ${placeholder}`} required={required} />
+        <label htmlFor={name} className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{label}</label>
+    </div>
+);
